c73150b9811f2f02604bc2659c0d3b01f4a108a0
agc9 <38140108+agc9@users.noreply.github.com>
2018-05-07 11:14:21 -0700

    added basic menus

--- src/main/java/edu/uw/sp18/tcss360a/group6/Application.java
+++ src/main/java/edu/uw/sp18/tcss360a/group6/Application.java
@@ -29,19 +29,32 @@ public class Application {
 
             User user = loginHandler.getUser();
 
-            //display valid user options based on user
-            console.printf("Choose an option\n");
-            console.printf("1. Exit\n");
-            console.printf("2. option 2\n");
+            bidderOptions(console);
+
 
             String line = null;
             while (line == null)
                 line = console.readLine();
             switch (line.toLowerCase()) {
                 case "1":
-                    stop();
+                    console.printf("you chose option 1...\n");
+                    console.printf("Enter 'b' to go back\n");
+                    line = console.readLine();
+                    switch (line.toLowerCase()) {
+                    case "b":
+                    	bidderOptions(console);
+                    	break;
+                    default:
+                    	console.printf("Invalid selection...\n");
+                    }
                     break;
                 case "2":
+                    console.printf("you chose option 2...\n");
+                    break;
+                case "3":
+                    console.printf("you chose option 2...\n");
+                    break;
+                case "4":
                     console.printf("you chose option 2...\n");
                     break;
                 default:
@@ -50,6 +63,24 @@ public class Application {
         }
     }
 
+    public void bidderOptions(Console console) {
+        //display valid user options based on user
+        console.printf("Choose an option\n");
+        console.printf("1. View brief overview of all auctions I have placed bids in.\n");
+        console.printf("2. View all items I have bid on in all auctions.\n");
+        console.printf("3. View in brief all auctions I can bid on.\n");
+        console.printf("4. Logout.\n");
+    }
+    
+    public void contactOptions(Console console) {
+    	//display valid contact options
+    	console.printf("Choose an option\n");
+    	console.printf("1. View brief overview of items in my auctions.\n");
+    	console.printf("2. Submit auction request.\n");
+    	console.printf("3. Add inventory for auction.\n");
+    	console.printf("4. Logout.\n");
+    	
+    }
     public void stop() {
         this.running = false;
     }
