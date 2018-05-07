a7657ee6173daa96467848f484b846c1e2f0036a
Evan Lindsay <evanlindsay@live.com>
2018-05-07 15:38:32 -0700

    Update LoginPrompt to use java.time api

--- src/main/java/edu/uw/sp18/tcss360a/group6/ui/LoginPrompt.java
+++ src/main/java/edu/uw/sp18/tcss360a/group6/ui/LoginPrompt.java
@@ -5,9 +5,8 @@ import edu.uw.sp18.tcss360a.group6.Context;
 import edu.uw.sp18.tcss360a.group6.io.Console;
 import edu.uw.sp18.tcss360a.group6.model.User;
 
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 /**
@@ -17,6 +16,9 @@ import java.util.List;
  * @version 5/1/2018
  */
 public class LoginPrompt extends AbstractPrompt {
+
+    private static final DateTimeFormatter LOGIN_DATE_FORMAT = DateTimeFormatter
+            .ofPattern("yyyy/MM/dd HH:mm:ss");
 
     public LoginPrompt(Context context) {
         super(context);
@@ -28,14 +30,13 @@ public class LoginPrompt extends AbstractPrompt {
         Console console = application.getConsole();
         List<User> users = application.getUserRepository().fetchAll();
 
-        DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
-        Date todaysDate = new Date();
+        LocalDateTime now = LocalDateTime.now();
         while (!context.has("user")) {
-            console.printf("Welcome to Auction Central " +
-                    dateFormat.format(todaysDate) +
-                    "\nEnter your user name: \n");
-            String name = console.readLine();
+            console.printfln("Welcome to Auction Central %s",
+                    LOGIN_DATE_FORMAT.format(now));
+            console.printf("Enter your user name: ");
 
+            String name = console.readLine();
             User user = users.stream()
                     .filter(u -> u.getUserName().equalsIgnoreCase(name))
                     .findFirst()
