6d1eec51e9e03bb823048d1fe786eaee5dc52f04
Josh Atherton <joshua9@uw.edu>
2018-05-07 23:01:39 -0700

    AuctionRequestPrompt working on execute method

--- src/main/java/edu/uw/sp18/tcss360a/group6/ui/AuctionRequestPrompt.java
+++ src/main/java/edu/uw/sp18/tcss360a/group6/ui/AuctionRequestPrompt.java
@@ -5,9 +5,12 @@ import edu.uw.sp18.tcss360a.group6.Context;
 import edu.uw.sp18.tcss360a.group6.io.Console;
 import edu.uw.sp18.tcss360a.group6.model.User;
 
+import java.time.LocalDate;
 import java.util.List;
 
 /**
+ * Submit a new auction to be added to the AuctionRepository.
+ *
  * @author Adam G. Cannon, Josh Atherton, Tam Bui, Evan Lindsay
  * @version 5/1/2018
  */
@@ -20,8 +23,26 @@ public class AuctionRequestPrompt extends AbstractPrompt {
 
     @Override
     public boolean execute(Context context) {
-        
-        
+        Application application = Application.getInstance();
+        Console console = application.getConsole();
+        // can add an auction
+        // get input to add a new auction
+        // create new auction object and add to auction repository
+
+        // public Auction(long id, long organizationId, LocalDate startDate)
+        long auctionID;
+        long organizationID;
+        LocalDate startDate;
+
+
+
+        //
+        /*
+        Pattern r = Pattern.compile("^\d{2}/\d{2}/\d{4}$");
+
+        String date = "05/07/2018";
+        date.matches("^\d{2}/\d{2}/\d{4}$");
+         */
         
         return true;
     }
