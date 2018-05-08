c1a9febb6dc9b7608a55ae49895fa20bfdd17700
Josh Atherton <joshua9@uw.edu>
2018-05-07 20:54:03 -0700

    BidderAuctionsPrompt wrote execute method

--- src/main/java/edu/uw/sp18/tcss360a/group6/ui/BidderAuctionsPrompt.java
+++ src/main/java/edu/uw/sp18/tcss360a/group6/ui/BidderAuctionsPrompt.java
@@ -3,11 +3,16 @@ package edu.uw.sp18.tcss360a.group6.ui;
 import edu.uw.sp18.tcss360a.group6.Application;
 import edu.uw.sp18.tcss360a.group6.Context;
 import edu.uw.sp18.tcss360a.group6.io.Console;
+import edu.uw.sp18.tcss360a.group6.model.Auction;
+import edu.uw.sp18.tcss360a.group6.model.Bid;
+import edu.uw.sp18.tcss360a.group6.model.Bidder;
 import edu.uw.sp18.tcss360a.group6.model.User;
 
 import java.util.List;
 
 /**
+ * Display a list of all auctions the user has placed bids in.
+ *
  * @author Adam G. Cannon, Josh Atherton, Tam Bui, Evan Lindsay
  * @version 5/1/2018
  */
@@ -20,7 +25,15 @@ public class BidderAuctionsPrompt extends AbstractPrompt {
 
     @Override
     public boolean execute(Context context) {
+        Application application = Application.getInstance();
+        Console console = application.getConsole();
+
+        Bidder user = context.get("user", Bidder.class);
+        List<Bid> bids = user.getPlacedBids();
         
+        for(Bid aBid : bids) {
+            console.printfln(aBid.toString());
+        }
         
         
         return true;
