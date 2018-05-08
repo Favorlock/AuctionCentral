ed6e195777d9310d2658781d2707384ef0b2b0d6
Josh Atherton <joshua9@uw.edu>
2018-05-07 23:14:05 -0700

    AuctionRequestPrompt working on execute method almost done

--- src/main/java/edu/uw/sp18/tcss360a/group6/ui/AuctionRequestPrompt.java
+++ src/main/java/edu/uw/sp18/tcss360a/group6/ui/AuctionRequestPrompt.java
@@ -3,6 +3,8 @@ package edu.uw.sp18.tcss360a.group6.ui;
 import edu.uw.sp18.tcss360a.group6.Application;
 import edu.uw.sp18.tcss360a.group6.Context;
 import edu.uw.sp18.tcss360a.group6.io.Console;
+import edu.uw.sp18.tcss360a.group6.model.Auction;
+import edu.uw.sp18.tcss360a.group6.model.AuctionRepository;
 import edu.uw.sp18.tcss360a.group6.model.User;
 
 import java.time.LocalDate;
@@ -29,12 +31,22 @@ public class AuctionRequestPrompt extends AbstractPrompt {
         // get input to add a new auction
         // create new auction object and add to auction repository
 
-        // public Auction(long id, long organizationId, LocalDate startDate)
         long auctionID;
         long organizationID;
         LocalDate startDate;
 
+        console.printfln("Enter the auction ID: "); //should be done dynamic
+        auctionID = Long.parseLong(console.readLine());
 
+        console.printfln("Enter organization ID: ");
+        organizationID = Long.parseLong(console.readLine());
+
+        console.printfln("Enter the auction date in the format: 2007-12-03 ");
+        startDate = LocalDate.parse(console.readLine());
+
+        Auction anAuction = new Auction(auctionID, organizationID, startDate);
+
+        AuctionRepository.add(anAuction);
 
         //
         /*
