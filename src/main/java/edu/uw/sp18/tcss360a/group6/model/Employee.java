8fdf6934a5e26637a0f82501fe1abfc3073becdc
Josh Atherton <joshua9@uw.edu>
2018-05-11 12:41:30 -0700

    added more data to .json repositories

--- src/main/java/edu/uw/sp18/tcss360a/group6/model/Employee.java
+++ src/main/java/edu/uw/sp18/tcss360a/group6/model/Employee.java
@@ -21,8 +21,45 @@ public class Employee extends AbstractUser {
         super(UserType.EMPLOYEE, id, userName);
     }
 
+    /*
+    As an employee of AuctionCentral, I want to change the maximum number of upcoming auctions that can be accepted by the system.
+        The maximum number of upcoming auctions must be a positive integer.
+            A non-positive integer is specified: fail
+            A positive integer is specified
+            A positive integer is specified that is greater than the number of existing auctions in the system
+    */
+    public void changeUpcomingAuctionsMax() {
 
+    }
 
+    /*
+    As an employee of AuctionCentral, I want a view in brief of all auctions that take place between any two dates, inclusive.
+        The second date is earlier than the first date : fail
+        The first and second dates are the same
+        The second date is at least one day later than the second date
+     */
+    public void viewAllAuctionsBetweenDates() {
+
+    }
+
+    /*
+    As an employee of AuctionCentral, I want a view in brief in chronological order of all auctions, past, present, and future.
+        There exists at least one auction in the past and at least one auction in the future.
+     */
+    public void viewAllAuctionsInOrder() {
+
+    }
+
+    /*
+    As an employee of AuctionCentral, I want to cancel an auction.
+        No auction can be cancelled that has any bids
+            The auction has no bids
+            The auction has one bid
+            The auction has many more than one bid
+     */
+    public void cancelAnAuction() {
+
+    }
 
 
 }
