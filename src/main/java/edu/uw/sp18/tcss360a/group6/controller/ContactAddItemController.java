1b435a745c35a6a88af8c93ae039dd57e98628d7
Adam Cannon <agc9@uw.edu>
2018-05-22 10:36:40 -0700

    more basic sub menus

--- src/main/java/edu/uw/sp18/tcss360a/group6/controller/ContactAddItemController.java
+++ src/main/java/edu/uw/sp18/tcss360a/group6/controller/ContactAddItemController.java
@@ -1,7 +1,21 @@
 package edu.uw.sp18.tcss360a.group6.controller;
 
+import edu.uw.sp18.tcss360a.group6.Bootstrap;
 import edu.uw.sp18.tcss360a.group6.FXApplication;
+import edu.uw.sp18.tcss360a.group6.model.Auction;
+import edu.uw.sp18.tcss360a.group6.util.ListViewCell;
+import javafx.beans.property.ListProperty;
+import javafx.beans.property.SimpleListProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
+import javafx.util.Callback;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * GUI for Contact person users add an Item to their Auction(s).
  *
@@ -9,7 +23,43 @@ import javafx.fxml.FXML;
  * @version 5/18/2018
  */
 public class ContactAddItemController {
+    @FXML
+    public ListView listView;
+    @FXML
     private FXApplication application = FXApplication.getInstance();
+    private ObservableList auctions = FXCollections.observableArrayList();
+    private ListProperty<String> listProperty = new SimpleListProperty<>();
+    public ContactAddItemController () {
+        super ();
+
+        Bootstrap bootstrap = new Bootstrap();
+        //TODO: dispaly only auctions associated with contact person
+        List<Auction> auctions = bootstrap.getAuctionRepository().fetchAllInChronologicalOrder();
+
+        listView = new ListView();
+
+        List<String> auctionString = new ArrayList<>();
+        //add the items to the list
+        for(Auction anAuction : auctions) {
+            auctionString.add(anAuction.toString());
+        }
+
+        this.auctions.setAll(auctionString);
+        listView.setItems(this.auctions);
+        listView.setCellFactory((Callback<ListView<String>, ListCell<String>>) listView -> new ListViewCell());
+
+        listView.setVisible(true);
+
+        displayAuctions(); // TODO: this should display them initially ????
+
+    }
+
+    @FXML
+    public void displayAuctions() {
+        listView.itemsProperty().bind(listProperty);
+        listProperty.set(FXCollections.observableArrayList(auctions));
+    }
+
     @FXML
     public void back() {
         application.getSceneController().activate("contactMain");
