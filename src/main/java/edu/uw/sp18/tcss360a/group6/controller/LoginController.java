a7e7aed1b22c0d0c3dc09369ecf743326d7d5889
Evan Lindsay <evanlindsay@live.com>
2018-05-22 16:19:17 -0700

    Add session creation to LoginController

--- src/main/java/edu/uw/sp18/tcss360a/group6/controller/LoginController.java
+++ src/main/java/edu/uw/sp18/tcss360a/group6/controller/LoginController.java
@@ -2,12 +2,14 @@ package edu.uw.sp18.tcss360a.group6.controller;
 
 import edu.uw.sp18.tcss360a.group6.Bootstrap;
 import edu.uw.sp18.tcss360a.group6.FXApplication;
+import edu.uw.sp18.tcss360a.group6.Session;
 import edu.uw.sp18.tcss360a.group6.model.User;
 import edu.uw.sp18.tcss360a.group6.model.UserType;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
+
 /**
  * GUI for users to login to the application.
  *
@@ -26,17 +28,18 @@ public class LoginController {
             FXApplication application = FXApplication.getInstance();
             User user = bootstrap.getUserRepository().fetchUser(this.userNameTextField.getText());
 
-            if (user != null) {
+            if (user != null && user.getType() != UserType.UNKNOWN) {
+                Session.create(user);
+
                 if (user.getType() == UserType.BIDDER) {
                     application.getSceneController().activate("bidderMain");
-
                 } else if (user.getType() == UserType.CONTACT_PERSON) {
                     application.getSceneController().activate("contactMain");
                 } else if (user.getType() == UserType.EMPLOYEE) {
                     application.getSceneController().activate("employeeMain");
                 }
             } else {
-                userNameTextField.setText("Invalid user, try again....");
+                this.userNameTextField.setText("Invalid user, try again....");
             }
         }
     }
