c6a2e965ed31954bbf8fe45cc90535ca51062d03
Adam Cannon <agc9@uw.edu>
2018-05-22 11:42:45 -0700

    all first level sub menus created

--- src/main/java/edu/uw/sp18/tcss360a/group6/controller/CancelBidController.java
+++ src/main/java/edu/uw/sp18/tcss360a/group6/controller/CancelBidController.java
@@ -1,18 +1,66 @@
 package edu.uw.sp18.tcss360a.group6.controller;
 
+import edu.uw.sp18.tcss360a.group6.Bootstrap;
 import edu.uw.sp18.tcss360a.group6.FXApplication;
+import edu.uw.sp18.tcss360a.group6.model.Auction;
+import edu.uw.sp18.tcss360a.group6.model.Bid;
+import edu.uw.sp18.tcss360a.group6.util.ListViewCell;
+import javafx.beans.property.ListProperty;
+import javafx.beans.property.SimpleListProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
+import javafx.util.Callback;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
- * GUI for users to cancel a bid on an Item in an Auction.
+ * GUI for Contact person users add an Item to their Auction(s).
  *
  * @author Adam G. Cannon, Josh Atherton, Tam Bui, Evan Lyndsay
  * @version 5/18/2018
  */
 public class CancelBidController {
+    @FXML
+    private ListView listView;
     private FXApplication application = FXApplication.getInstance();
+    private ObservableList observableBids = FXCollections.observableArrayList();
+
+    private ListProperty<String> listProperty = new SimpleListProperty<>();
+   public CancelBidController () {
+        Bootstrap bootstrap = new Bootstrap();
+        //TODO: display only bids associated with bidder
+        List<Bid> bids = bootstrap.getBidRepository().fetchAll();
+
+        listView = new ListView();
+
+        List<String> bidsString = new ArrayList<>();
+        //add the items to the list
+        for(Bid anBid : bids) {
+            bidsString.add(anBid.toString()); //.getItem()
+        }
+
+        this.observableBids.setAll(bidsString);
+        listView.setItems(this.observableBids);
+        listView.setCellFactory((Callback<ListView<String>, ListCell<String>>) listView -> new ListViewCell());
+
+        listView.setVisible(true);
+
+        displayBids(); // TODO: this should display them initially ????
+
+    }
+
+    @FXML
+    public void displayBids() {
+        listView.itemsProperty().bind(listProperty);
+        listProperty.set(FXCollections.observableArrayList(observableBids));
+    }
     @FXML
     public void back() {
-        application.getSceneController().activate("bidderMain");
+        application.getSceneController().activate("contactMain");
     }
     @FXML
     public void logout() {
--- src/main/java/edu/uw/sp18/tcss360a/group6/controller/EmployeeCancelAnAuctionController.java
+++ src/main/java/edu/uw/sp18/tcss360a/group6/controller/EmployeeCancelAnAuctionController.java
@@ -1,19 +1,68 @@
 package edu.uw.sp18.tcss360a.group6.controller;
 
+import edu.uw.sp18.tcss360a.group6.Bootstrap;
 import edu.uw.sp18.tcss360a.group6.FXApplication;
+import edu.uw.sp18.tcss360a.group6.model.Auction;
+import edu.uw.sp18.tcss360a.group6.util.ListViewCell;
+import javafx.beans.property.ListProperty;
+import javafx.beans.property.SimpleListProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
+import javafx.util.Callback;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
- * GUI for employee to attempt to cancel an auction.
+ * GUI for Contact person users add an Item to their Auction(s).
  *
  * @author Adam G. Cannon, Josh Atherton, Tam Bui, Evan Lyndsay
  * @version 5/18/2018
  */
 public class EmployeeCancelAnAuctionController {
-
+    @FXML
+    public ListView listView;
+    @FXML
     private FXApplication application = FXApplication.getInstance();
+    private ObservableList auctions = FXCollections.observableArrayList();
+    private ListProperty<String> listProperty = new SimpleListProperty<>();
+    public EmployeeCancelAnAuctionController () {
+        super ();
+
+        Bootstrap bootstrap = new Bootstrap();
+        //TODO: dispaly only auctions associated with contact person
+        List<Auction> auctions = bootstrap.getAuctionRepository().fetchAllInChronologicalOrder();
+
+        listView = new ListView();
+
+        List<String> auctionString = new ArrayList<>();
+        //add the items to the list
+        for(Auction anAuction : auctions) {
+            auctionString.add(anAuction.toString());
+        }
+
+        this.auctions.setAll(auctionString);
+        listView.setItems(this.auctions);
+        listView.setCellFactory((Callback<ListView<String>, ListCell<String>>) listView -> new ListViewCell());
+
+        listView.setVisible(true);
+
+        displayAuctions(); // TODO: this should display them initially ????
+
+    }
+
+    @FXML
+    public void displayAuctions() {
+        listView.itemsProperty().bind(listProperty);
+        listProperty.set(FXCollections.observableArrayList(auctions));
+    }
+
     @FXML
     public void back() {
-        application.getSceneController().activate("employeeMain");
+        application.getSceneController().activate("contactMain");
     }
     @FXML
     public void logout() {
