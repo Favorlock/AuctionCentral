69314914b0ff669fb7f46b93f78f0bd6417d0f7b
Josh Atherton <joshua9@uw.edu>
2018-05-21 19:00:13 -0700

    ContactViewAuctionsController displays all auctions

--- src/main/java/edu/uw/sp18/tcss360a/group6/controller/ContactViewAuctionsController.java
+++ src/main/java/edu/uw/sp18/tcss360a/group6/controller/ContactViewAuctionsController.java
@@ -1,13 +1,16 @@
 package edu.uw.sp18.tcss360a.group6.controller;
 
 
+import edu.uw.sp18.tcss360a.group6.Bootstrap;
 import edu.uw.sp18.tcss360a.group6.FXApplication;
+import edu.uw.sp18.tcss360a.group6.model.Auction;
 import edu.uw.sp18.tcss360a.group6.util.ListViewCell;
 import javafx.beans.property.ListProperty;
 import javafx.beans.property.SimpleListProperty;
 import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 import javafx.collections.ObservableList;
+import javafx.scene.control.Button;
 import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
 import javafx.util.Callback;
@@ -25,31 +28,37 @@ import java.util.List;
 public class ContactViewAuctionsController {
     @FXML
     public ListView listView;
+    @FXML
     private FXApplication application = FXApplication.getInstance();
     private  ObservableList names = FXCollections.observableArrayList();
-
     private ListProperty<String> listProperty = new SimpleListProperty<>();
 
     public ContactViewAuctionsController () {
         super ();
 
+        Bootstrap bootstrap = new Bootstrap();
+        List<Auction> auctions = bootstrap.getAuctionRepository().fetchAllInChronologicalOrder();
+
         listView = new ListView();
 
         List<String> name = new ArrayList<>();
-        name.add("Adam");
-        name.add("Alfred");
-        name.add("Albert");
+        //add the items to the list
+        for(Auction anAuction : auctions) {
+            name.add(anAuction.toString());
+        }
+
         names.setAll(name);
         listView.setItems(names);
         listView.setCellFactory((Callback<ListView<String>, ListCell<String>>) listView -> new ListViewCell());
 
         listView.setVisible(true);
 
+        displayAuctions(); // TODO: this should display them initially ????
+
     }
 
     @FXML
     public void displayAuctions() {
-        System.out.println("--- button works ---");
         listView.itemsProperty().bind(listProperty);
         listProperty.set(FXCollections.observableArrayList(names));
     }
