6906863de50160929fc434bac47e48f4d497743e
Evan Lindsay <evanlindsay@live.com>
2018-05-22 18:55:45 -0700

    Load fxml scenes on activation rather than on application start

--- src/main/java/edu/uw/sp18/tcss360a/group6/SceneController.java
+++ src/main/java/edu/uw/sp18/tcss360a/group6/SceneController.java
@@ -1,31 +1,40 @@
 package edu.uw.sp18.tcss360a.group6;
 
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
 public class SceneController {
 
-    private Map<String, Parent> screenMap = new HashMap<>();
+    private Map<String, String> scenePathMap = new HashMap<>();
     private Scene primaryScene;
 
     public SceneController(Scene primaryScene) {
         this.primaryScene = primaryScene;
     }
 
-    public void addScreen(String name, Parent parent) {
-        this.screenMap.put(name, parent);
+    public void addScreen(String name, String path) {
+        this.scenePathMap.put(name, path);
     }
 
     public void removeScreen(String name) {
-        this.screenMap.remove(name);
+        this.scenePathMap.remove(name);
     }
 
     public void activate(String name) {
-
-        this.primaryScene.setRoot(this.screenMap.get(name));
+        String path = this.scenePathMap.get(name);
+        if (path != null) {
+            try {
+                Parent parent = FXMLLoader.load(getClass().getResource(path));
+                this.primaryScene.setRoot(parent);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
 }
