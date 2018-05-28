6e9a8a35b3aa6fc202816d0afa423f6f0a1be9ac
Josh Atherton <joshua9@uw.edu>
2018-05-28 07:49:50 -0700

    added contactperson test

---
+++ src/test/java/edu/uw/sp18/tcss360a/group6/model/ContactPersonTest.java
@@ -0,0 +1,41 @@
+package edu.uw.sp18.tcss360a.group6.model;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author Adam G. Cannon, Josh Atherton, Tam Bui, Evan Lindsay
+ * @version 5/11/2018
+ */
+public class ContactPersonTest {
+    ContactPerson contactPerson;
+    long contactPersonID;
+    long contactPersonOrganizationID;
+    String contactPersonUserName;
+    @Before
+    public void setUp() {
+        contactPersonID = 1;
+        contactPersonOrganizationID = 1;
+        contactPersonUserName = "Joseph";
+        contactPerson = new ContactPerson(contactPersonID,
+                contactPersonOrganizationID, contactPersonUserName);
+
+    }
+
+    @Test
+    public void getOrganizationID_TestRightID_True() {
+        assertEquals(contactPersonOrganizationID, contactPerson.getOrganizationId());
+    }
+    @Test
+    public void getOrganizationID_TestWrongID_false() {
+        assertNotEquals(contactPersonOrganizationID, contactPerson.getOrganizationId());
+    }
+
+    @Test
+    public void getOrganization_TestGettingRightOrganizationName_True() {
+        assertEquals("UNICEF", contactPerson.getOrganization().getName());
+    }
+
+}
